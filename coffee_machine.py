decaf940a974cd785ca928a82592ed460f989a19
Voren4Today <64384136+Voren4Today@users.noreply.github.com>
2020-04-29 23:19:57 -0400

    Coffee machine task

---
+++ coffee_machine.py
@@ -0,0 +1,8 @@
+num_cups = int(input("How many cups of coffee do you need?"))
+water_in_ml = 200 * num_cups
+milk_in_ml = 50 * num_cups
+coffee_beans_in_gram = 15 * num_cups
+print("For " + str(num_cups) + "cups of coffe, you need:")
+print(str(water_in_ml) +" ml of water,")
+print(str(milk_in_ml) +" ml of milk, and")
+print(str(coffee_beans_in_gram) +" grams of coffee beans.")
